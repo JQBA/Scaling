a522ed38f96e8b1cc3836a848ac7f55e6b2be0a3
JQBA <jmaster1727@gmail.com>
2024-08-16 20:05:22 -0500

    Expanded menu + moved menu junk into folders

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time. Hiii noah U//w//U
 func _ready():
 	Object.new()# Replace with function body.
 
