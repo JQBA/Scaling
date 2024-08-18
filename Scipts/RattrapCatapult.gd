6e197f462ae0df56f08d9c6e981f3e4a229f8f05
JQBA <jmaster1727@gmail.com>
2024-08-17 23:11:58 -0500

    working mouse trap :3

--- Scipts/RattrapCatapult.gd
+++ Scipts/RattrapCatapult.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	var impulseAmount = Vector2(200, 200)
-	body.apply_impulse(impulseAmount)
-	
+	#var impulseAmount = Vector2(200, 200)
+	#body.apply_impulse(impulseAmount)
+	body.velocity.y += -600
+	body.velocity.x += 600
