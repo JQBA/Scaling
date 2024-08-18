ef10bec35561eec756a4056cf10335ad12c60240
VladimirGlutenn <magicsean03@gmail.com>
2024-08-17 21:44:24 -0500

    Main Menu Edits

--- MainMenus/main_menu.gd
+++ MainMenus/main_menu.gd
@@ -17,26 +17,3 @@ func _on_start_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
-
-
-#moves button for start
-func _on_start_mouse_entered():
-	$"start/start img".frame = 1
-
-func _on_start_mouse_exited():
-	$"start/start img".frame = 0
-
-#moves button for quit
-func _on_quit_mouse_entered():
-	$"Quit/quit img".frame = 1
-
-func _on_quit_mouse_exited():
-	$"Quit/quit img".frame = 0
-
-
-#same for options 
-func _on_options_mouse_entered():
-		$"Options/options img".frame = 1 
-
-func _on_options_mouse_exited():
-		$"Options/options img".frame = 0
