66b61510b50fe031cb7148ac74bf6945a6e7d070
JQBA <jmaster1727@gmail.com>
2024-08-18 17:32:00 -0500

    Fluffed out levels added collision to all needed tiles

--- Scipts/PlayerScipt.gd
+++ Scipts/PlayerScipt.gd
@@ -25,9 +25,16 @@ func _physics_process(delta):
 	if isKing:
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+		
 		# Get the input direction and handle the movement/deceleration.
+		
+		
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("MoveLeft", "MoveRight")
+		if velocity.x > 0:
+			$Sprite2D.flip_h = true
+		elif velocity.x < 0:
+			$Sprite2D.flip_h = false
 		if direction:
 			velocity.x = direction * SPEED
 		else:
