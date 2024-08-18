379f276b5e6ab110b8da2a5e205b4e9be2081600
Noah Hall <njh200011@gmail.com>
2024-08-18 17:23:41 -0500

    Main menu music, collsion clean ups

--- Scipts/EndDoor.gd
+++ Scipts/EndDoor.gd
@@ -2,17 +2,10 @@ extends Area2D
 
 var EndScreen = preload("res://GamePlayObjects/EndLevelScreen.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 	var EndScreenSpawn = EndScreen.instantiate()
-	body.queue_free()
-	get_tree().get_current_scene().add_child(EndScreenSpawn)
+	if body.isKing:
+		body.queue_free()
+		get_tree().get_current_scene().add_child(EndScreenSpawn)
+	else: 
+		body.queue_free()
--- Scipts/PlayerScipt.gd
+++ Scipts/PlayerScipt.gd
@@ -8,9 +8,6 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var RatSize = 1
-
-func _ready():
-	print(isKing)
 	
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
