76e79f8e713569f51e42c1b4081f8e5649f97579
JQBA <jmaster1727@gmail.com>
2024-08-18 17:01:32 -0500

    began laying out groundwork for levels

--- Scipts/PlayerScipt.gd
+++ Scipts/PlayerScipt.gd
@@ -10,6 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var RatSize = 1
 
 func _ready():
+	
 	print(isKing)
 	
 func _unhandled_key_input(event: InputEvent) -> void:
