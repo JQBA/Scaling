c4ac0efaa91560478fb6496328b06c4f858ba9d1
Noah Hall <njh200011@gmail.com>
2024-08-17 19:43:33 -0500

    Resetting level
    Can now reset the level by pressing r at any given point

--- LevelScript.gd
+++ LevelScript.gd
@@ -7,4 +7,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ResetLevel"):
+		get_tree().reload_current_scene()
