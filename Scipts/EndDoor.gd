bb1a74c8dd6bc57ac4ec1a003ddbb33bf3620739
Noah Hall <njh200011@gmail.com>
2024-08-19 18:20:52 -0500

    work on level 2, some code clean up

--- Scipts/EndDoor.gd
+++ Scipts/EndDoor.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 var EndScreen = preload("res://GamePlayObjects/EndLevelScreen.tscn")
+@export var nextLevel:String
 var killCount = 0
 func _on_body_entered(body):
 	var EndScreenSpawn = EndScreen.instantiate()
 	if body.isKing:
 		body.queue_free()
-		get_tree().get_current_scene().add_child(EndScreenSpawn)
+		get_tree().change_scene_to_file(nextLevel)
 	else: 
 		killCount+=1
 		body.queue_free()
